ef23cab3c3b7cf154168bc4d26e48cf287df160e
Syed Qaim raza <162017596+Qaim9980@users.noreply.github.com>
2026-08-06 23:35:19 +0500

    Configure next.config.js for GitHub Pages
    Added basePath and assetPrefix for GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   output: 'export',
+  trailingSlash: true,
+
+  // Required for GitHub Pages project repositories
+  basePath: '/waseem-potfolio',
+  assetPrefix: '/waseem-potfolio',
+
   images: {
     unoptimized: true,
     domains: ['via.placeholder.com', 'lh3.googleusercontent.com'],
